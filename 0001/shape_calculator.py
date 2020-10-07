bc9859d45740a96dec93983a7ec1eb5bae90c524
Charles Keenan <keenan.charles@protonmail.com>
2020-10-07 11:43:51 -0700

    Square methods and tests completed

--- 0001/shape_calculator.py
+++ 0001/shape_calculator.py
@@ -54,28 +54,26 @@ class Rectangle:
     
     def __str__(self):
         return f"Rectangle(width={self.width}, height={self.height})"
-        """
-        Additionally, if an instance of a Rectangle is represented as a string, 
-        it should look like: Rectangle(width=5, height=10)
-        """
-        pass
     
     
 
 class Square(Rectangle):
-    pass
-
-"""
-The Square class should be a subclass of Rectangle. When a Square object is created, 
-a single side length is passed in. The __init__ method should store the side length 
-in both the width and height attributes from the Rectangle class.
-
-The Square class should be able to access the Rectangle class methods but should 
-also contain a set_side method. If an instance of a Square is represented as a 
-string, it should look like: Square(side=9)
-
-Additionally, the set_width and set_height methods on the Square class should 
-set both the width and height.
-"""
+    def __init__(self, side):
+        self.width = side
+        self.height = side
+    
+    def set_side(self, new_side):
+        self.width = new_side
+        self.height = new_side
+        
+    def set_width(self, new_width):
+        self.width, self.height = new_width, new_width
+        
+    
+    def set_height(self, new_height):
+        self.height, self.width = new_height, new_height
+        
+    def __str__(self):
+        return f"Square(side={self.width})"
 
 
